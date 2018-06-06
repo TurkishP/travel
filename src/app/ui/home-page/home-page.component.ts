4d5cc46e5f240e1f662279094a74faf4e4ab484c
2lchanm10826 <okok1501@naver.com>
2018-06-07 02:49:43 +0900

    해시태그 시도..

--- src/app/ui/home-page/home-page.component.ts
+++ src/app/ui/home-page/home-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { NewLocaPopupComponent } from './new-loca-popup/new-loca-popup.component';
 import { LocationService } from '../../core/location.service';
 import { Observable } from 'rxjs';
@@ -17,7 +17,7 @@ import * as firebase from 'firebase/app';
 export class HomePageComponent implements OnInit {
   locations: Observable<any[]>;
   UID: string;
-  constructor(    
+  constructor(
     public dialog: MatDialog,
     private loca: LocationService,
     private auth: AuthService,
@@ -27,21 +27,41 @@ export class HomePageComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.getlocations();    
+    this.getlocations();
 
-    this.afAuth.authState.subscribe(user=>{
-      if(user) this.UID = user.uid
+    this.afAuth.authState.subscribe(user => {
+      if (user) this.UID = user.uid
     })
+    /// this.gethashtag();
+    this.add();
   }
 
+  add() {
+    console.log("h8i");
 
-  getlocations(){
+    var content = document.getElementById('contents').innerHTML;
+    var splitedArray = content.split(' ');
+    var linkedContent = '';
+    console.log("테그테그");
+    for (var word in splitedArray) {
+      word = splitedArray[word];
+      if (word.indexOf('#') == 0) {
+        word = '<a href=\'링크\'>' + word + '</a>';
+        console.log("테그테그");
+      }
+      linkedContent += word + ' ';
+    }
+    document.getElementById('contents').innerHTML = linkedContent;
+
+  }
+
+  getlocations() {
     this.locations = this.loca.getLocations();
   }
 
   openDialog() {
     const dialogRef = this.dialog.open(NewLocaPopupComponent, {
-      data:{},
+      data: {},
       height: '570px',
       width: '600px'
     });
@@ -52,11 +72,11 @@ export class HomePageComponent implements OnInit {
 
   }
 
-  deleteLocation(id:string){
+  deleteLocation(id: string) {
     this.loca.deleteLocation(id);
   }
 
-  like(location_id: string){
+  like(location_id: string) {
     console.log(location_id, this.UID)
     this.afs.collection('users').doc(this.UID).collection('like').doc(location_id).set({
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
