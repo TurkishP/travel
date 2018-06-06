c186e1ea05d2f7044c7493e73a832641b4ef69fc
Joseph Kim <turkishprince@Josephui-MacBook-Pro.local>
2018-06-07 01:00:50 +0900

    minor fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,6 @@ import { environment } from '../environments/environment';
 import { PlanComponent } from './plan/plan.component';
 
 import { MaterialModule } from './material.module';
-import { MytravelComponent } from './mytravel/mytravel.component';
 // import { NewLocaPopupComponent } from './ui/home-page/new-loca-popup/new-loca-popup.component';
 
 
@@ -30,7 +29,6 @@ import { MytravelComponent } from './mytravel/mytravel.component';
   declarations: [
     AppComponent,
     PlanComponent,
-    MytravelComponent,
     
   ],
   imports: [
