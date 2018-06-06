d48b282161a91a3c5ad81096c1c24a0a6f6431fc
TurkishP <jk_9397@hotmail.com>
2018-06-06 20:34:50 +0900

    basic like function added

--- src/app/ui/home-page/home-page.component.ts
+++ src/app/ui/home-page/home-page.component.ts
@@ -4,6 +4,10 @@ import { NewLocaPopupComponent } from './new-loca-popup/new-loca-popup.component
 import { LocationService } from '../../core/location.service';
 import { Observable } from 'rxjs';
 import { AuthService } from '../../core/auth.service';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestore } from 'angularfire2/firestore';
+import * as firebase from 'firebase/app';
+
 
 @Component({
   selector: 'home-page',
@@ -12,15 +16,22 @@ import { AuthService } from '../../core/auth.service';
 })
 export class HomePageComponent implements OnInit {
   locations: Observable<any[]>;
-
+  UID: string;
   constructor(    
     public dialog: MatDialog,
     private loca: LocationService,
     private auth: AuthService,
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore,
+
   ) { }
 
   ngOnInit() {
     this.getlocations();    
+
+    this.afAuth.authState.subscribe(user=>{
+      if(user) this.UID = user.uid
+    })
   }
 
 
@@ -43,5 +54,12 @@ export class HomePageComponent implements OnInit {
 
   deleteLocation(id:string){
     this.loca.deleteLocation(id);
+  }
+
+  like(location_id: string){
+    console.log(location_id, this.UID)
+    this.afs.collection('users').doc(this.UID).collection('like').doc(location_id).set({
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
   }
 }
